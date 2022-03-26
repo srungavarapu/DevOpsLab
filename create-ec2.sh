6772d37c957f76954d16de07e61f530f17274574
s.raidu <s.raidu@hotmail.com>
2022-03-26 13:33:49 +0800

    Ansible Projects Sync to GitHub

--- create-ec2.sh
+++ create-ec2.sh
@@ -32,3 +32,8 @@ if [ "$1" == "all" ]; then
 else
   create_ec2
 fi
+
+# Deploy EC2 from the AWS CLI
+# aws ec2 run-instances --image-id ami-0bb6af715826253bf --security-group-ids sg-008147de9733254d3 --instance-type t2.small
+# Running AWS Command Line Interface using Jenkins build job
+# /usr/local/bin/aws ec2 run-instances --image-id ami-0bb6af715826253bf --security-group-ids sg-008147de9733254d3 --instance-type t2.small
---
+++ ec2-launch.sh
@@ -0,0 +1,2 @@
+# Running AWS Command Line Interface using Jenkins build job
+/usr/local/bin/aws ec2 run-instances --image-id ami-0bb6af715826253bf --security-group-ids sg-008147de9733254d3 --instance-type t2.small
